152bbe0b01959ab9655edb3f19f725541d029434
Jagoba Peruarena Martinez <Jagoba Peruarena Martinez@HZ114822.H012108.NET>
2023-01-24 08:44:39 +0100

    commitdos

--- arbolesclases/arbolees/arbolesMain.java
+++ arbolesclases/arbolees/arbolesMain.java
@@ -1,11 +1,12 @@
 package arbolees;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 
 import arbolees.*;
 
 public class arbolesMain {
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, SQLException {
 		gestorArboles gf = new gestorArboles();
 		gf.run();
 		
--- arbolesclases/arbolees/gestorArboles.java
+++ arbolesclases/arbolees/gestorArboles.java
@@ -1,6 +1,8 @@
 package arbolees;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class gestorArboles {
@@ -8,8 +10,8 @@ public class gestorArboles {
 	static final String BBDD="arboles";
 	static final String USERNAME="root";
 	static final String PASSWORD="";
-	static Connection conecion;
-	public static void run() {
+	static Connection conexion;
+	public static void run() throws SQLException {
 		final int OPCION_UNO = 1;
 		final int OPCION_DOS = 2;
 		final int OPCION_TRES = 3;
@@ -18,25 +20,35 @@ public class gestorArboles {
 		int opcion_menu=1;
 		Scanner miTeclado = new Scanner(System.in);
 		while(opcion_menu!=OPCION_SALIR) {
-			System.out.println("Menu:\n "+OPCION_UNO+".Multiplo de 3 \n "+OPCION_DOS+".Fibonacci \n "+OPCION_TRES+".Factorial \n "+OPCION_SALIR+".Salir");
-			opcion_menu = miTeclado.nextInt();
-			
+			System.out.println("Menu:\n "+OPCION_UNO+".Insertar arbol \n "+OPCION_DOS+".eliminar arbol \n "+OPCION_TRES+".modificar informacion del arbol \n "+OPCION_CUATRO+".visualizar arboles \n "+OPCION_SALIR+".Salir");
+			opcion_menu = Integer.parseInt(miTeclado.nextLine());
+			Statement st=conexion.createStatement();
 			
 			switch (opcion_menu) {
 			case OPCION_UNO:
-				System.out.println("La opcion de Multiplo de 3");
-				
+				System.out.println("La opcion de insertar arbol");
+				System.out.println("Di el nombre del arbol");
+				String nomArbol = miTeclado.nextLine();
+				System.out.println("Di el nombre cientifico del arbol");
+				String nomCArbol = miTeclado.nextLine();
+				System.out.println("Di el habitat del arbol");
+				String habitat = miTeclado.nextLine();
+				System.out.println("Di la altura del arbol");
+				int altura = Integer.parseInt(miTeclado.nextLine());
+				System.out.println("Di el origen del arbol");
+				String origen = miTeclado.nextLine();
+				st.execute("INSERT INTO animales (nombre_comun,nombre_cientifico,habitat,altura,origen) VALUES ('"+nomArbol+"','"+nomCArbol+"','"+habitat+"','"+altura+"','"+origen+"')");
 				break;
 			case OPCION_DOS:
-				System.out.println("La opcion de Fibonacci");
+				System.out.println("La opcion de eliminar arbol");
 				
 				break;
 			case OPCION_TRES:
-				System.out.println("La opcion de Factorial");
+				System.out.println("La opcion de modificar informacion del arbol");
 				
 				break;
 			case OPCION_CUATRO:
-				System.out.println("La opcion de Factorial");
+				System.out.println("La opcion de visualizar arboles");
 				
 				break;
 				
