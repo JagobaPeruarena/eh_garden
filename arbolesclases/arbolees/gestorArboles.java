07905aa03626583d7c3aaef469b5caca397b9e49
Jagoba Peruarena Martinez <Jagoba Peruarena Martinez@HZ114822.H012108.NET>
2023-01-24 09:27:18 +0100

    cuartocommit

--- arbolesclases/arbolees/gestorArboles.java
+++ arbolesclases/arbolees/gestorArboles.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
+
 
 public class gestorArboles {
 	static final String HOST="jdbc:mysql://localhost/";
@@ -19,8 +21,7 @@ public class gestorArboles {
 		final int OPCION_CUATRO =4;
 		final int OPCION_SALIR = 0;
 		int opcion_menu=1;
-		int numArbolD;
-		String sent;
+	
 		Scanner miTeclado = new Scanner(System.in);
 		while(opcion_menu!=OPCION_SALIR) {
 			System.out.println("Menu:\n "+OPCION_UNO+".Insertar arbol \n "+OPCION_DOS+".eliminar arbol \n "+OPCION_TRES+".modificar informacion del arbol \n "+OPCION_CUATRO+".visualizar arboles \n "+OPCION_SALIR+".Salir");
@@ -57,41 +58,51 @@ public class gestorArboles {
 		System.out.println("ADIOS");
 	}
 	private static void visualizarArboles(Statement st) throws SQLException {
-		String select="SELECT * FROM arboles ";
-		st.execute(select);
-		ResultSet resultado=st.executeQuery(select);
+		PreparedStatement pt = conexion.prepareStatement("SELECT * FROM arboles ");
+		
+	
+		ResultSet resultado=pt.executeQuery();
 		while(resultado.next()) {
 			System.out.println(resultado.getInt(1)+"-"+resultado.getString(2)+"-"+resultado.getString(3)+"-"+resultado.getString(4)+"-"+resultado.getInt(5)+"-"+resultado.getString(6));
 		}
 	}
 	private static void eliminarArboles(Scanner miTeclado, Statement st) throws SQLException {
 		int numArbolD;
-		String sent;
+		String sent="DELETE FROM arboles WHERE id=?";
+		PreparedStatement pt = conexion.prepareStatement(sent);
 		System.out.println("Di la id del arbol que quieras eliminar");
 		numArbolD = Integer.parseInt(miTeclado.nextLine());
-		sent="DELETE FROM arboles WHERE id='"+numArbolD+"'";
-		st.execute(sent);
+		pt.setInt(1, numArbolD);
+		pt.execute();
+		
 	}
 	private static void actualizarArboles(Scanner miTeclado, Statement st) throws SQLException {
 		int numArbolD;
-		String sent;
+		String sent= "UPDATE arboles SET ?=? WHERE id=?";
+		PreparedStatement pt = conexion.prepareStatement(sent);
 		System.out.println("Di la id del arbol que quieras modificar");
 		numArbolD = Integer.parseInt(miTeclado.nextLine());
+		pt.setInt(3, numArbolD);
 		System.out.println("Di el factor a cambiar: nombre_comun,nombre_cientifico,habitat,altura,origen");
 		String factorC=miTeclado.nextLine().toLowerCase();
+		pt.setString(1, factorC);
 		if (factorC.equals("altura")) {
 			System.out.println("Di la nueva altura");
 			int altC = Integer.parseInt(miTeclado.nextLine());
-			sent="UPDATE arboles SET "+factorC+"='"+altC+"' WHERE id="+numArbolD+"";
+			
+			pt.setInt(2, altC);
 		}else {
 			System.out.println("Di el nuevo atributo");
 			String atribC = miTeclado.nextLine();
-			sent="UPDATE arboles SET "+factorC+"='"+atribC+"' WHERE id="+numArbolD+"";
+			pt.setString(2, atribC);
 		}
+		pt.executeUpdate();
 		
-		st.executeUpdate(sent);
+		
 	}
 	private static void insertar_arbol(Scanner miTeclado, Statement st) throws SQLException {
+		String sent ="INSERT INTO arboles (nombre_comun,nombre_cientifico,habitat,altura,origen) VALUES (?,?,?,?,?)";
+		PreparedStatement pt = conexion.prepareStatement(sent);
 		System.out.println("Di el nombre del arbol");
 		String nomArbol = miTeclado.nextLine();
 		System.out.println("Di el nombre cientifico del arbol");
@@ -102,7 +113,13 @@ public class gestorArboles {
 		int altura = Integer.parseInt(miTeclado.nextLine());
 		System.out.println("Di el origen del arbol");
 		String origen = miTeclado.nextLine();
-		st.execute("INSERT INTO arboles (nombre_comun,nombre_cientifico,habitat,altura,origen) VALUES ('"+nomArbol+"','"+nomCArbol+"','"+habitat+"','"+altura+"','"+origen+"')");
+		
+		pt.setString(1, nomArbol);
+		pt.setString(2, nomCArbol);
+		pt.setString(3, habitat);
+		pt.setInt(4, altura);
+		pt.setString(5, origen);
+		pt.execute();
 	}
 		
 	}
